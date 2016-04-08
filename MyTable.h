9dbd0a6e85ba52037ea91188112da9c5b7a60636
Kirito <1362050620@qq.com>
2016-04-08 13:47:57 +0800

    Create MyTable.h
    This is the header of MyTable. Include this to get MyTable features.

---
+++ MyTable.h
@@ -0,0 +1,17 @@
+/** MyTable On GitHub
+    https://github.com/Kiritow/MyTable
+*/
+#ifndef HC_TECH_GITHUB_PROJECT_MYTABLE_H
+#define HC_TECH_GITHUB_PROJECT_MYTABLE_H
+
+/**
+MyTable
+MyTable is a light open-source self-made cross-platform database software.
+Written in C/C++.
+Some APIs are provided to operate the database. ( Listed in MyTable.hpp )
+*/
+
+#include "MyTable.hpp"
+#include "MyTable.cpp"
+
+#endif /// End of HC_TECH_GITHUB_PROJECT_MYTABLE_H
